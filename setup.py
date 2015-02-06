80a6118c6f20caf4a248102f6535a24ceb4eeead
Jeremy Brudvik <jeremy@brudvik.me>
2015-02-05 21:19:01 -0800

    Add PyPI config files

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='yahooscraper',
+    version=0.0.1,
+    description='Utilities for scraping Yahoo pages',
+    license='MIT',
+    author='Jeremy Brudvik',
+    author_email='jeremy@brudvik.me',
+    url='https://github.com/jbrudvik/yahooscraper',
+    keywords=['scrape', 'screen', 'web', 'Yahoo', 'login', 'fantasy'],
+    packages=['yahooscraper', 'yahooscraper.fantasy'],
+    install_requires=[
+        'beautifulsoup4 >= 4.3.2'
+    ])
