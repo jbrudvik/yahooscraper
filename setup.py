54c3e81a9f814a95570380c98b4b42f8bd15e27d
Jeremy Brudvik <jeremy@brudvik.me>
2015-02-27 17:51:49 -0800

    Bump to version 0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except:
 
 setup(
     name='yahooscraper',
-    version='0.2.0',
+    version='0.2.1',
     description='Utilities for scraping Yahoo pages',
     long_description=long_description,
     license='MIT',
